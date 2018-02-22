0983311f3bbac54f5171e8632b3423e3567463b7
Bruno Oliveira <bruno@esss.com.br>
2018-02-22 15:27:44 -0300

    Use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
     long_description=read('README.rst'),
     py_modules=['pytest_replay'],
     install_requires=['pytest>=3.0.0'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Pytest',
