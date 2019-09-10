8f948dcc7283a40aff043e96502d7068a4073a5e
Bruno Oliveira <bruno@esss.com.br>
2019-09-10 15:29:06 -0300

    Run pre-commit

--- pytest_replay.py
+++ pytest_replay.py
@@ -5,25 +5,25 @@ from glob import glob
 
 
 def pytest_addoption(parser):
-    group = parser.getgroup('replay')
+    group = parser.getgroup("replay")
     group.addoption(
-        '--replay-record-dir',
-        action='store',
-        dest='replay_record_dir',
+        "--replay-record-dir",
+        action="store",
+        dest="replay_record_dir",
         default=None,
-        help='Directory to write record files to reproduce runs.'
+        help="Directory to write record files to reproduce runs.",
     )
     group.addoption(
-        '--replay',
-        action='store',
-        dest='replay_file',
+        "--replay",
+        action="store",
+        dest="replay_file",
         default=None,
-        help='Use a replay file to run the tests from that file only',
+        help="Use a replay file to run the tests from that file only",
     )
 
 
 class ReplayPlugin(object):
-    BASE_SCRIPT_NAME = '.pytest-replay'
+    BASE_SCRIPT_NAME = ".pytest-replay"
 
     def __init__(self, config):
         self.dir = config.getoption('replay_record_dir')
@@ -59,11 +59,11 @@ class ReplayPlugin(object):
             self.append_test_to_script(nodeid)
 
     def pytest_collection_modifyitems(self, items, config):
-        replay_file = config.getoption('replay_file')
+        replay_file = config.getoption("replay_file")
         if not replay_file:
             return
 
-        with io.open(replay_file, 'r', encoding='UTF-8') as f:
+        with io.open(replay_file, "r", encoding="UTF-8") as f:
             nodeids = set(x.strip() for x in f.readlines())
 
         remaining = []
@@ -79,11 +79,11 @@ class ReplayPlugin(object):
             items[:] = remaining
 
     def append_test_to_script(self, nodeid):
-        suffix = self.suffix_sep + self.xdist_worker_name
+        suffix = "-" + self.xdist_worker_name if self.xdist_worker_name else ""
         fn = os.path.join(self.dir, self.BASE_SCRIPT_NAME + suffix + self.ext)
-        flag = 'a' if os.path.isfile(fn) else 'w'
-        with io.open(fn, flag, encoding='UTF-8') as f:
-            f.write(nodeid + u'\n')
+        flag = "a" if os.path.isfile(fn) else "w"
+        with io.open(fn, flag, encoding="UTF-8") as f:
+            f.write(nodeid + u"\n")
             self.written_nodeids.add(nodeid)
 
     @property
@@ -92,10 +92,10 @@ class ReplayPlugin(object):
 
 
 def pytest_configure(config):
-    if config.getoption('replay_record_dir') or config.getoption('replay_file'):
-        config.pluginmanager.register(ReplayPlugin(config), 'replay-writer')
+    if config.getoption("replay_record_dir") or config.getoption("replay_file"):
+        config.pluginmanager.register(ReplayPlugin(config), "replay-writer")
 
 
 def pytest_report_header(config):
-    if config.getoption('replay_record_dir'):
-        return 'replay dir: {}'.format(config.getoption('replay_record_dir'))
+    if config.getoption("replay_record_dir"):
+        return "replay dir: {}".format(config.getoption("replay_record_dir"))
--- setup.py
+++ setup.py
@@ -8,42 +8,38 @@ from setuptools import setup
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
 setup(
-    name='pytest-replay',
-    version='0.0.0',
-    author='Bruno Oliveira',
-    author_email='bruno@esss.com.br',
-    maintainer='Bruno Oliveira',
-    maintainer_email='bruno@esss.com.br',
-    license='MIT',
-    url='https://github.com/ESSS/pytest-replay',
-    description='Saves previous test runs and allow re-execute previous pytest runs '
-                'to reproduce crashes or flaky tests',
-    long_description=read('README.rst'),
-    py_modules=['pytest_replay'],
-    install_requires=['pytest>=3.0.0'],
+    name="pytest-replay",
+    version="0.0.0",
+    author="Bruno Oliveira",
+    author_email="bruno@esss.com.br",
+    maintainer="Bruno Oliveira",
+    maintainer_email="bruno@esss.com.br",
+    license="MIT",
+    url="https://github.com/ESSS/pytest-replay",
+    description="Saves previous test runs and allow re-execute previous pytest runs "
+    "to reproduce crashes or flaky tests",
+    long_description=read("README.rst"),
+    py_modules=["pytest_replay"],
+    install_requires=["pytest>=3.0.0"],
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
-    entry_points={
-        'pytest11': [
-            'replay = pytest_replay',
-        ],
-    },
+    entry_points={"pytest11": ["replay = pytest_replay"]},
 )
